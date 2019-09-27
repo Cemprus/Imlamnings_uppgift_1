824a5ef43c25fd3491e0a31dff95a96651af4e7a
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2019-09-27 10:04:41 +0200

    Adding new Class

---
+++ src/Klasser/NewClass.java
@@ -0,0 +1,13 @@
+package Klasser;
+
+public class NewClass {
+    private String name;
+
+    public NewClass() { }
+    public NewClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
